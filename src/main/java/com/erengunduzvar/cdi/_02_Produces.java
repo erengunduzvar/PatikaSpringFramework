49c8873139e7037d2c68f91949f7fefd021b8637
Eren Gunduzvar <190201058@uzem.education>
2022-10-06 15:59:35 +0300

    _produces_

---
+++ src/main/java/com/erengunduzvar/cdi/_02_Produces.java
@@ -0,0 +1,26 @@
+package com.erengunduzvar.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _02_Produces {
+
+    @Named(value = "producetuto")
+    @ApplicationScoped
+
+
+    @Produces
+    public List<String> getList(){
+        List<String> liste = new ArrayList<>();
+        liste.add("Html5");
+        liste.add("css3");
+        liste.add("js");
+        liste.add("react");
+        liste.add("angular");
+        return liste;
+
+    }
+}
